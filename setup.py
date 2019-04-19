4db81465e1dd4604e1ebf8034b3f0644f1f310cc
ikronskun <liukun@onesight.cn>
2019-04-19 16:29:05 +0800

    feat(:rocket: test publish to pypi.):

--- setup.py
+++ setup.py
@@ -65,5 +65,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
