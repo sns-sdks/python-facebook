0134c1f7219cd6eebeb2fa592b765b63dd6255e5
ikronskun <merle.liukun@gmail.com>
2019-08-19 18:51:59 +0800

    feat(:construction: Add simple instagram ratelimit.):

--- pyfacebook/ratelimit.py
+++ pyfacebook/ratelimit.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import time
 
 try:
     from json.decoder import JSONDecodeError
@@ -9,9 +10,31 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
+def get_interval(usage_count):
+    interval = 1
+    if usage_count < 20:
+        interval = 1
+    elif usage_count < 50:
+        interval = 3
+    elif usage_count < 80:
+        interval = 5
+    elif usage_count < 90:
+        interval = 60 * 5
+        logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
+    elif usage_count < 100:
+        interval = 60 * 10
+        logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
+    elif usage_count > 100:
+        interval = 60 * 20
+        logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
+
+    return interval
+
+
 class RateLimit(object):
     """
-    app's rate limit message
+    app's rate limit message.
+    Refer: https://developers.facebook.com/docs/graph-api/overview/rate-limiting#application-level-rate-limiting
     """
     DEFAULT_TIME_WINDOW = 60 * 60
 
@@ -37,26 +60,64 @@ class RateLimit(object):
             self.total_time,
             self.total_cputime
         )
-        interval = 1
-        if usage_count < 20:
-            interval = 1
-        elif usage_count < 50:
-            interval = 3
-        elif usage_count < 80:
-            interval = 5
-        elif usage_count < 90:
-            interval = 60 * 5
-            logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
-        elif usage_count < 100:
-            interval = 60 * 10
-            logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
-        elif usage_count > 100:
-            interval = 60 * 20
-            logging.debug("App usage is arrive {0}%, need sleep {1} seconds".format(usage_count, interval))
+        return get_interval(usage_count)
 
-        return interval
-
+    @property
     def info(self):
         return "Current Limit is RateLimit(call_count={0},total_cputime={1},total_time={2})".format(
             self.call_count, self.total_cputime, self.total_time
+        )
+
+
+class InstagramRateLimit(object):
+    """
+    Simple RateLimit check.
+    Refer: https://developers.facebook.com/docs/graph-api/overview/rate-limiting#instagram-rate-limiting
+    """
+    DEFAULT_TIME_WINDOW = 60 * 60
+
+    def __init__(self, **kwargs):
+        self.type = 'instagram'
+        self.call_count = kwargs.get('call_count', 0)
+        self.total_cputime = kwargs.get('total_cputime', 0)
+        self.total_time = kwargs.get('total_time', 0)
+        self.reset_at = kwargs.get('reset_at', 0)
+
+    def set_limit(self, headers, instagram_business_id):
+        x_business_use_case_usage = headers.get('x-business-use-case-usage')
+        if x_business_use_case_usage:
+            try:
+                usage_data = json.loads(x_business_use_case_usage)
+                if instagram_business_id in usage_data:
+                    data = usage_data[instagram_business_id]
+                else:
+                    data = {'call_count': 0, 'total_cputime': 0, 'total_time': 0}
+            except (TypeError, JSONDecodeError):
+                logging.debug("Can not get rate limit info for {0}. Usage: {1}".format(
+                    instagram_business_id, x_business_use_case_usage
+                ))
+                data = {'call_count': 0, 'total_cputime': 0, 'total_time': 0}
+            self.call_count = data['call_count']
+            self.total_cputime = data['total_cputime']
+            self.total_time = data['total_time']
+            now = int(time.time())
+            self.reset_at = now + data['estimated_time_to_regain_access']
+
+    def get_sleep_interval(self):
+        now = int(time.time())
+        if self.reset_at > now:
+            need_sleep = self.reset_at - now
+            logging.debug("Api call still limited. Maybe remain {} seconds".format(need_sleep))
+            return need_sleep
+        usage_count = max(
+            self.call_count,
+            self.total_time,
+            self.total_cputime
+        )
+        return get_interval(usage_count)
+
+    @property
+    def info(self):
+        return "Current Limit is RateLimit(call_count={0},total_cputime={1},total_time={2},reset_at={3})".format(
+            self.call_count, self.total_cputime, self.total_time, self.reset_at
         )
