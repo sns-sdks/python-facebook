a59d4d537c041a38ba4eec74140a4e6c2170ae41
IkarosKun <merle.liukun@gmail.com>
2019-12-14 23:26:32 +0800

    test(model): :white_check_mark: add access token model tests

--- pyfacebook/model/__init__.py
+++ pyfacebook/model/__init__.py
@@ -1,7 +1,10 @@
+from .access_token import AccessToken, AuthAccessToken
 from .base import BaseModel
 from .page import Page
 
 __all__ = [
+    "AccessToken",
+    "AuthAccessToken",
     "BaseModel",
     "Page"
 ]
