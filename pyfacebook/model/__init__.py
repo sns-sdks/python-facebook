42af5b183eed4b570839a4c8fccb33cedfdd7562
IkarosKun <merle.liukun@gmail.com>
2019-12-16 23:18:34 +0800

    test(model): :white_check_mark: add test for comment model

--- pyfacebook/model/__init__.py
+++ pyfacebook/model/__init__.py
@@ -1,5 +1,6 @@
 from .access_token import AccessToken, AuthAccessToken
 from .base import BaseModel
+from .comment import Comment
 from .page import Page, PageCategory
 from .post import Post
 
@@ -7,6 +8,8 @@ __all__ = [
     "AccessToken",
     "AuthAccessToken",
     "BaseModel",
+    "Comment",
     "Page",
     "PageCategory",
+    "Post",
 ]
