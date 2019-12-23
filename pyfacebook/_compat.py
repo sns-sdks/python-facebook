6636d161558bf00f79d7a3abc667e24dc3b188f2
ikronskun <merle.liukun@gmail.com>
2019-12-23 10:30:02 +0800

    style(call): :art: use dot properties

--- pyfacebook/_compat.py
+++ pyfacebook/_compat.py
@@ -8,10 +8,10 @@ import sys
 _ver = sys.version_info
 
 #: Python 2.x?
-is_py2 = (_ver[0] == 2)
+is_py2 = (_ver.major == 2)
 
 #: Python 3.x?
-is_py3 = (_ver[0] == 3)
+is_py3 = (_ver.major == 3)
 
 if is_py2:
     str = unicode  # noqa
