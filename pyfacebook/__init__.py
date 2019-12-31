bedd533ce00697131b3dff07b2a8c70d79ac37e9
ikronskun <merle.liukun@gmail.com>
2019-12-31 11:35:50 +0800

    feat(instagram): :sparkles: modify ig professional discovery api and tests

--- pyfacebook/__init__.py
+++ pyfacebook/__init__.py
@@ -3,6 +3,6 @@
 from __future__ import absolute_import
 
 from .api.facebook import Api  # noqa
-from .api.instagram import InstagramApi  # noqa
+from .api.instagram import IgProApi  # noqa
 from .error import *  # noqa
 from .models import *  # noqa
