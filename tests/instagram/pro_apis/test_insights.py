b37a20d1fb145c3c0f00fa8fc522a9c246bc17f1
IkarosKun <merle.liukun@gmail.com>
2020-01-08 22:52:27 +0800

    test(insights): :white_check_mark: add tests for get media insights

--- tests/instagram/pro_apis/test_insights.py
+++ tests/instagram/pro_apis/test_insights.py
@@ -14,6 +14,10 @@ class ApiMediaTest(unittest.TestCase):
         USER_INSIGHTS = json.loads(f.read().decode("utf-8"))
     with open(BASE_PATH + "user_insights_2.json", "rb") as f:
         USER_INSIGHTS_2 = json.loads(f.read().decode("utf-8"))
+    with open(BASE_PATH + "media_album_insights.json", "rb") as f:
+        MEDIA_ALBUM_INSIGHTS = json.loads(f.read().decode("utf-8"))
+    with open(BASE_PATH + "media_photo_insights.json", "rb") as f:
+        MEDIA_PHOTO_INSIGHTS = json.loads(f.read().decode("utf-8"))
 
     def setUp(self):
         self.instagram_business_id = "123456"
@@ -49,3 +53,31 @@ class ApiMediaTest(unittest.TestCase):
             self.assertEqual(len(res_json), 2)
             self.assertEqual(res_json[1]["name"], "reach")
             self.assertEqual(len(res_json[1]["values"]), 10)
+
+    def testGetMediaInsights(self):
+        media_album_id = "12345"
+        media_photo_id = "67890"
+        with responses.RequestsMock() as m:
+            m.add("GET", self.BASE_URL + media_album_id + "/insights", json=self.MEDIA_ALBUM_INSIGHTS)
+            m.add("GET", self.BASE_URL + media_photo_id + "/insights", json=self.MEDIA_PHOTO_INSIGHTS)
+
+            media_album_insights = self.api.get_media_insights(
+                media_id=media_album_id,
+                metrics=[
+                    "carousel_album_engagement", "carousel_album_impressions",
+                    "carousel_album_reach", "saved", "video_views"
+                ]
+            )
+            self.assertEqual(len(media_album_insights), 5)
+            self.assertEqual(media_album_insights[0].period, "lifetime")
+            self.assertEqual(media_album_insights[0].values[0].value, 138)
+
+            media_photo_insights = self.api.get_media_insights(
+                media_id=media_photo_id,
+                metrics=[
+                    "engagement", "impressions", "reach", "saved"
+                ],
+                return_json=True
+            )
+            self.assertEqual(len(media_photo_insights), 4)
+            self.assertEqual(media_photo_insights[0]["values"][0]["value"], 65)
