d5ff3399bef4d16d81d26d330e8279b76d4e87d8
Ikaros <merle.liukun@gmail.com>
2020-09-12 19:31:55 +0800

    feat(models): :sparkles: add video model

---
+++ tests/facebook/models/test_video.py
@@ -0,0 +1,25 @@
+import json
+import unittest
+
+import pyfacebook.models as models
+
+
+class PostModelTest(unittest.TestCase):
+    BASE_PATH = "testdata/facebook/models/videos/"
+
+    with open(BASE_PATH + 'video.json', 'rb') as f:
+        VIDEO_INFO = json.loads(f.read().decode('utf-8'))
+
+    def testVideo(self):
+        m: models.Video = models.Video.new_from_json_dict(self.VIDEO_INFO)
+
+        self.assertEqual(m.id, "969222676905304")
+        self.assertEqual(m.created_time, "2020-09-12T09:53:06+0000")
+        self.assertEqual(len(m.content_tags), 2)
+        self.assertEqual(m.format[0].height, 229)
+        self.assertEqual(m.place.id, "406584169736960")
+        self.assertEqual(m.place.location.zip, "100037")
+        self.assertEqual(m.status.video_status, "ready")
+
+        self.assertEqual(m.privacy.value, "EVERYONE")
+        self.assertFalse(m.is_crosspost_video)
