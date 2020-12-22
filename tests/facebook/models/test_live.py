bc888a3725ea95c101bf358d57f5da798aaf0334
ikaroskun <merle.liukun@gmail.com>
2020-12-22 19:21:10 +0800

    feat(model): :sparkles: add model for live video

---
+++ tests/facebook/models/test_live.py
@@ -0,0 +1,19 @@
+import json
+import unittest
+
+import pyfacebook.models as models
+
+
+class LiveVideoModelTest(unittest.TestCase):
+    BASE_PATH = "testdata/facebook/models/live/"
+
+    with open(BASE_PATH + 'live_video.json', 'rb') as f:
+        LIVE_VIDEO_INFO = json.loads(f.read().decode('utf-8'))
+
+    def testLiveVideo(self):
+        m: models.LiveVideo = models.LiveVideo.new_from_json_dict(self.LIVE_VIDEO_INFO)
+
+        self.assertEqual(m.id, "178598660650827")
+        self.assertEqual(m.status, "LIVE")
+        self.assertEqual(m.ingest_streams[0].stream_id, "0")
+        self.assertEqual(m.ingest_streams[0].stream_health.video_bitrate, 2511983)
