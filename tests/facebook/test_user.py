01c10c5d27a880cfa2fa7892a44bdf4db1b7181a
ikaros <merle.liukun@gmail.com>
2021-07-07 00:35:42 +0800

    docs(readme): :memo: remove old readme

--- tests/facebook/test_user.py
+++ tests/facebook/test_user.py
@@ -2,10 +2,7 @@
     Tests for user api
 """
 
-import pytest
 import responses
-
-from pyfacebook.exceptions import LibraryError
 
 
 def test_get_info(helpers, fb_api):
@@ -15,11 +12,15 @@ def test_get_info(helpers, fb_api):
         m.add(
             method=responses.GET,
             url=f"https://graph.facebook.com/{fb_api.version}/{uid}",
-            json=helpers.load_json("testdata/facebook/apidata/users/user.json")
+            json=helpers.load_json("testdata/facebook/apidata/users/user.json"),
         )
 
         user = fb_api.user.get_info(user_id=uid)
         assert user.id == uid
 
-        user_json = fb_api.user.get_info(user_id=uid, fields="id,first_name,last_name,middle_name,name,name_format,picture,short_name", return_json=True)
+        user_json = fb_api.user.get_info(
+            user_id=uid,
+            fields="id,first_name,last_name,middle_name,name,name_format,picture,short_name",
+            return_json=True,
+        )
         assert user_json["id"] == uid
