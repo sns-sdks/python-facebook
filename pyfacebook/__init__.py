8e643f6d6ba5a854dac692f190327663e36f9542
ikaroskun <merle.liukun@gmail.com>
2021-07-07 13:27:26 +0800

    fix(version): :bug: fix version

--- pyfacebook/__init__.py
+++ pyfacebook/__init__.py
@@ -3,3 +3,5 @@ from pyfacebook.ratelimit import RateLimitHeader, RateLimit, PercentSecond
 from pyfacebook.exceptions import PyFacebookException, FacebookError, LibraryError
 from pyfacebook.api import GraphAPI
 from pyfacebook.api.facebook.client import FacebookApi
+
+__version__ = "0.9.3"
