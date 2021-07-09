c43e6728d5b614fca44d20ceba3cc520f809c585
ikaroskun <merle.liukun@gmail.com>
2021-07-09 18:52:10 +0800

    feat(api): :sparkles: add api for batch posts

--- pyfacebook/api/facebook/resource/post.py
+++ pyfacebook/api/facebook/resource/post.py
@@ -2,7 +2,7 @@
     Apis for post.
 """
 
-from typing import Optional, Union
+from typing import Dict, Optional, Union
 
 import pyfacebook.utils.constant as const
 from pyfacebook.api.facebook.resource.base import BaseResource
@@ -13,9 +13,9 @@ from pyfacebook.utils.params_utils import enf_comma_separated
 class FacebookPost(BaseResource):
     def get_info(
         self,
-        post_id: Optional[str] = None,
+        post_id: Optional[str],
         fields: Optional[Union[str, list, tuple]] = None,
-        return_json=False,
+        return_json: bool = False,
     ) -> Union[Post, dict]:
         """
         Get information about a Facebook post
@@ -38,3 +38,35 @@ class FacebookPost(BaseResource):
             return data
         else:
             return Post.new_from_json_dict(data=data)
+
+    def get_batch(
+        self,
+        ids: Optional[Union[str, list, tuple]],
+        fields: Optional[Union[str, list, tuple]] = None,
+        return_json: bool = False,
+    ) -> Union[Dict[str, Post], dict]:
+        """
+        Get batch posts information by ids.
+
+        :param ids: IDs for the posts.
+        :param fields: Comma-separated id string for data fields which you want.
+            You can also pass this with an id list, tuple.
+        :param return_json: Set to false will return a dataclass for post.
+            Or return json data. Default is false.
+        :return: Posts information.
+        """
+
+        ids = enf_comma_separated(field="ids", value=ids)
+
+        if fields is None:
+            fields = const.POST_PUBLIC_FIELDS + const.POST_CONNECTIONS_SUMMERY_FIELDS
+
+        data = self.client.get_objects(
+            ids=ids, fields=enf_comma_separated(field="fields", value=fields)
+        )
+        if return_json:
+            return data
+        else:
+            return {
+                post_id: Post.new_from_json_dict(item) for post_id, item in data.items()
+            }
--- tests/facebook/test_post.py
+++ tests/facebook/test_post.py
@@ -31,3 +31,31 @@ def test_post_info(helpers, fb_api):
             post_id=post_id, fields="id,created_time,message", return_json=True
         )
         assert post_json["id"] == post_id
+
+
+def test_batch_posts(helpers, fb_api):
+    ids = ["19292868552_10158347294088553", "19292868552_10158345715593553"]
+
+    with responses.RequestsMock() as m:
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{fb_api.version}",
+            json=helpers.load_json(
+                "testdata/facebook/apidata/posts/multi_posts_data.json"
+            ),
+        )
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{fb_api.version}",
+            json=helpers.load_json(
+                "testdata/facebook/apidata/posts/multi_posts_data_fields.json"
+            ),
+        )
+
+        data = fb_api.post.get_batch(ids=ids)
+        assert ids[0] in data.keys()
+
+        data_json = fb_api.post.get_batch(
+            ids=ids, fields="id,message,created_time,updated_time", return_json=True
+        )
+        assert data_json[ids[0]]["id"] == ids[0]
