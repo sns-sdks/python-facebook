4ec87a31ba27cf69b44c40996d430529abb0acb4
ikaroskun <merle.liukun@gmail.com>
2021-08-19 21:11:13 +0800

    test(tests): :white_check_mark: update tests for media edges

--- tests/instagram_business/test_media.py
+++ tests/instagram_business/test_media.py
@@ -49,3 +49,76 @@ def test_get_batch(helpers, api):
             return_json=True,
         )
         assert medias_json[media_ids[0]]["id"] == media_ids[0]
+
+
+def test_get_comments(helpers, api):
+    media_id = "17846368219941692"
+    with responses.RequestsMock() as m:
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{api.version}/{media_id}/comments",
+            json=helpers.load_json(
+                "testdata/instagram/apidata/medias/comments_p1.json"
+            ),
+        )
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{api.version}/{media_id}/comments",
+            json=helpers.load_json(
+                "testdata/instagram/apidata/medias/comments_p2.json"
+            ),
+        )
+
+        comments = api.media.get_comments(media_id=media_id)
+        assert len(comments.data) == 4
+        assert comments.data[0].id == "17858154961981086"
+
+        comments_json = api.media.get_comments(
+            media_id=media_id,
+            return_json=True,
+        )
+        assert comments_json["data"][0]["id"] == "17892250648466172"
+
+
+def test_get_children(helpers, api):
+    media_id = "17846368219941692"
+    with responses.RequestsMock() as m:
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{api.version}/{media_id}/children",
+            json=helpers.load_json("testdata/instagram/apidata/medias/children.json"),
+        )
+
+        children = api.media.get_children(media_id=media_id)
+        assert len(children.data) == 2
+        assert children.data[0].media_type == "IMAGE"
+
+        children_json = api.media.get_children(
+            media_id=media_id,
+            return_json=True,
+        )
+        assert children_json["data"][0]["id"] == "17924318443748703"
+
+
+def test_get_insights(helpers, api):
+    media_id = "17846368219941692"
+    with responses.RequestsMock() as m:
+        m.add(
+            method=responses.GET,
+            url=f"https://graph.facebook.com/{api.version}/{media_id}/insights",
+            json=helpers.load_json("testdata/instagram/apidata/medias/insights.json"),
+        )
+
+        insights = api.media.get_insights(
+            media_id=media_id,
+            metric="impressions,reach,saved",
+        )
+        assert len(insights.data) == 3
+        assert insights.data[0].name == "impressions"
+
+        insights_json = api.media.get_insights(
+            media_id=media_id,
+            metric="impressions,reach,saved",
+            return_json=True,
+        )
+        assert insights_json["data"][0]["name"] == "impressions"
