8534ff780dd4ec08aec44d129798d7609e433662
ikaroskun <merle.liukun@gmail.com>
2021-08-19 14:24:04 +0800

    feat(media): :sparkles: add get user media

--- pyfacebook/api/instagram_business/resource/user.py
+++ pyfacebook/api/instagram_business/resource/user.py
@@ -8,6 +8,7 @@ import pyfacebook.utils.constant as const
 from pyfacebook.api.base_resource import BaseResource
 from pyfacebook.models.ig_business_models import (
     IgBusUser,
+    IgBusMediaResponse,
     IgBusDiscoveryUserResponse,
     IgBusDiscoveryUserMediaResponse,
     IgBusPublishLimitResponse,
@@ -202,3 +203,44 @@ class IGBusinessUser(BaseResource):
             return data
         else:
             return IgBusInsightsResponse.new_from_json_dict(data)
+
+    def get_media(
+        self,
+        fields: Optional[Union[str, list, tuple]] = None,
+        since: Optional[str] = None,
+        until: Optional[str] = None,
+        count: Optional[int] = 10,
+        limit: Optional[int] = 10,
+        return_json=False,
+    ) -> Union[IgBusMediaResponse, dict]:
+        """
+        Get user's media. This only can return max 10k medias.
+
+        :param fields: Comma-separated id string for data fields which you want.
+            You can also pass this with an id list, tuple.
+        :param since: A Unix timestamp or strtotime data value that points to the start of data.
+        :param until: A Unix timestamp or strtotime data value that points to the end of data.
+        :param count: The total count for you to get data.
+        :param limit: Each request retrieve objects count.
+            It should no more than 100. Default is None will use api default limit.
+        :param return_json: Set to false will return a dataclass for IgBusMediaResponse.
+            Or return json data. Default is false.
+        :return: Media response information.
+        """
+
+        if fields is None:
+            fields = const.IG_BUSINESS_MEDIA_PUBLIC_FIELDS
+
+        data = self.client.get_full_connections(
+            object_id=self.client.instagram_business_id,
+            connection="media",
+            count=count,
+            limit=limit,
+            fields=enf_comma_separated(field="fields", value=fields),
+            since=since,
+            until=until,
+        )
+        if return_json:
+            return data
+        else:
+            return IgBusMediaResponse.new_from_json_dict(data)
