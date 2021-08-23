8bf93ce3f206593f67effd536756678ee1ad6380
ikaroskun <merle.liukun@gmail.com>
2021-08-23 18:23:57 +0800

    feat(models): :sparkles: add models for basic media

--- pyfacebook/models/ig_basic_models.py
+++ pyfacebook/models/ig_basic_models.py
@@ -3,9 +3,10 @@
 """
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
 
 from pyfacebook.models.base import BaseModel, field
+from pyfacebook.models.extensions import Paging
 
 
 @dataclass
@@ -20,3 +21,43 @@ class IgBasicUser(BaseModel):
     account_type: Optional[str] = field(repr=True)
     media_count: Optional[int] = field()
     username: Optional[str] = field(repr=True)
+
+
+@dataclass
+class IgBasicMediaChild(BaseModel):
+    """
+    A class representing the Basic display media child.
+
+    Refer: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
+    """
+
+    id: Optional[str] = field(repr=True, compare=True)
+    caption: Optional[str] = field()
+    media_type: Optional[str] = field()
+    media_url: Optional[str] = field(repr=True)
+    permalink: Optional[str] = field()
+    thumbnail_url: Optional[str] = field()
+    timestamp: Optional[str] = field()
+
+
+@dataclass
+class IgBasicMediaChildren(BaseModel):
+    """
+    A class representing the Basic display media children data.
+
+    Refer: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media/children
+    """
+
+    data: Optional[List[IgBasicMediaChild]] = field()
+    paging: Optional[Paging] = field()
+
+
+@dataclass
+class IgBasicMedia(IgBasicMediaChild):
+    """
+    A class representing the Basic display media.
+
+    Refer: https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields
+    """
+
+    username: Optional[str] = field()
