485f73e5c76df03ddd55b0dd58fc6a364b6a5525
ikaroskun <merle.liukun@gmail.com>
2024-10-08 16:23:30 +0800

    feat(tests): :white_check_mark: update tests for specified scope

--- tests/test_threads_graph_api.py
+++ tests/test_threads_graph_api.py
@@ -10,7 +10,7 @@ from pyfacebook import ThreadsGraphAPI
 def test_threads_get_authorization_url():
     api = ThreadsGraphAPI(app_id="id", app_secret="secret", oauth_flow=True)
 
-    url, state = api.get_authorization_url()
+    url, state = api.get_authorization_url(scope=["threads_basic"])
     assert (
         url
         == "https://threads.net/oauth/authorize?response_type=code&client_id=id&redirect_uri=https%3A%2F%2Flocalhost%2F&scope=threads_basic&state=PyFacebook"
@@ -29,7 +29,7 @@ def test_threads_exchange_user_access_token(helpers):
             json=helpers.load_json("testdata/base/threads_user_token.json"),
         )
 
-        r = api.exchange_user_access_token(response=resp)
+        r = api.exchange_user_access_token(response=resp, scope=["threads_basic"])
         assert r["access_token"] == "THQVJ..."
 
 
